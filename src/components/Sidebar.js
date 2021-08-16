cfea3f0765e3f581eebf2ce4c4fafb810b91d151
fahadprod <fahadahmadmd@gmail.com>
2021-08-16 12:17:04 +0500

    bug fix

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,11 +2,10 @@ import React from "react"
 import links from "../constants/links"
 import socialLinks from "../constants/social_links"
 import { Link } from "gatsby"
-import { FaTimes } from "react-icons/fa"
 const Sidebar = ({ isOpen, toggleSidebar }) => {
   return (
     <aside className={isOpen ? "sidebar show-sidebar" : "sidebar"}>
-      <button type="button" className="close-btn" onClick={toggleSidebar}><FaTimes /></button>
+      <button type="button" className="close-btn" onClick={toggleSidebar}>Open</button>
       <div className="side-container">
         <ul className={isOpen ? "sidebar-links" : null}>
           {links.map(link => {
